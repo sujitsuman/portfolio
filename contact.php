5f18a29d56af0e7c527375ed6c2c211a5330622d
sujit suman <sumansujit2009@gmail.com>
2019-08-31 16:09:50 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,23 +1,20 @@
- <?php
- //$to = "info@sujitsuman.com";
- $to = "sumansujit2009@gmail.com";
- //$subject = "Contact Us";
- $name = $_REQUEST['name'] ;
- $email = $_REQUEST['email'] ;
- $subject = $_REQUEST['subject'] ;
- $message = $_REQUEST['message'] ;
- // $headers = "From: $email";
- $headers = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'From:  ' . $name . ' <' . $email .'>' . " \r\n" .
-            'Reply-To: '.  $fromEmail . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
- $sent = mail($to, $subject, $message, $headers, "-f sumansujit2009@gmail.com") ;
- if($sent)
- {print "Your mail was sent successfully";
- $url = 'index.html';
- echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';
- }
- else
- {print "We encountered an error sending your mail"; }
- ?>
+<?php 
+if(isset($_POST['submit'])){
+    $to = "email@example.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
