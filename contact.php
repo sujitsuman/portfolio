5838174e2e6b0f4adac54ecf5c01df23558eb2be
Sujit-wittyparrot <sujit@wittyparrot.com>
2019-09-02 14:13:54 +0530

    Changes Done

--- contact.php
+++ contact.php
@@ -1,18 +1,18 @@
- <?php 
+ <?php
  //$to = "info@sujitsuman.com";
- $to = "sumansujit2009@gmail.com"; 
+ $to = "sumansujit2009@gmail.com";
  //$subject = "Contact Us";
- $name = $_REQUEST['name'] ; 
- $email = $_REQUEST['email'] ; 
+ $name = $_REQUEST['name'] ;
+ $email = $_REQUEST['email'] ;
  $subject = $_REQUEST['subject'] ;
- $message = $_REQUEST['message'] ; 
- $headers = "From: $email"; 
+ $message = $_REQUEST['message'] ;
+ $headers = "From: $email";
  $sent = mail($to, $subject, $message, $headers) ; 
- if($sent) 
- {print "Your mail was sent successfully"; 
+ if($sent)
+ {print "Your mail was sent successfully";
  $url = 'index.html';
  echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';
  }
- else 
+ else
  {print "We encountered an error sending your mail"; }
- ?> 
+ ?>
